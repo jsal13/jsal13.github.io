0f1e487a45e1afb22fa49d5851f2ff23d3243e13
James Salvatore <jimmy.c.salvatore@gmail.com>
2023-09-27 23:02:48 -0500

    chore: bootstrap color update;

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '/src/style.css'
+import '/src/bootstrap-custom.scss'
+
 
 const app = createApp(App)
 app.use(router)
